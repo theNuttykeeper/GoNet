9454b5c675751f4c104b09df6d578eff4d30c348
MichaelShep <mgs538@york.ac.uk>
2022-04-12 10:34:58 +0100

    Added some more operations to tensors and added the testing for these new operations

--- tensor/Tensor.go
+++ tensor/Tensor.go
@@ -1,5 +1,7 @@
 package tensor
 
+import "fmt"
+
 //Defines the representation of a tensor which will be used for all network operations
 type Tensor struct {
 	data []float64
@@ -15,14 +17,43 @@ func (tensor *Tensor) GetData() []float64 {
 	return tensor.data
 }
 
-//In place element-wise addition of another tensor object to the current one
-func (tensor *Tensor) Add(other *Tensor) {
+//In place element-wise addition of another tensor object to the current one - returns an error if subtraction could not be performed
+func (tensor *Tensor) Add(other *Tensor) error {
 	//Can only add tensors of the same size
 	if len(tensor.data) != len(other.data) {
-		return
+		return fmt.Errorf("cannot perform addition on two tensors of different dimensions, dimensions provided were: %d and %d", len(tensor.data), len(tensor.data))
 	}
 
-	for i, element := range tensor.data {
-		tensor.data[i] = element + other.data[i]
+	for i := range tensor.data {
+		tensor.data[i] += other.data[i]
 	}
+
+	return nil
+}
+
+//In place element-wise subtraction of another tensor object to the current one - returns an error if subtraction could not be performed
+func (tensor *Tensor) Subtract(other *Tensor) error {
+	//Can only add tensors of the same size
+	if len(tensor.data) != len(other.data) {
+		return fmt.Errorf("cannot perform subtraction on two tensors of different dimensions, dimensions provided were: %d and %d", len(tensor.data), len(tensor.data))
+	}
+
+	for i := range tensor.data {
+		tensor.data[i] -= other.data[i]
+	}
+
+	return nil
+}
+
+//Returns the dot product to two tensors - returns an error if dot product cannot be performed
+func (tensor *Tensor) DotProduct(other *Tensor) (float64, error) {
+	if len(tensor.data) != len(other.data) {
+		return -1.0, fmt.Errorf("cannot perform dot product on two tensors of unequal dimensions, dimensions provided were: %d and %d", len(tensor.data), len(tensor.data))
+	}
+
+	var product float64 = 0
+	for i, element := range tensor.data {
+		product += element * other.data[i]
+	}
+	return product, nil
 }
--- tensor/tensor_test.go
+++ tensor/tensor_test.go
@@ -6,13 +6,74 @@ import (
 	"github.com/theNuttykeeper/GoNet/utils"
 )
 
-//Test for the add functionality of tensors
-func TestAdd(t *testing.T) {
+//Test for the add functionality of tensors with valid inputs
+func TestAddValid(t *testing.T) {
 	tensor1 := NewTensor([]float64{1.0, 10.0, 5.0})
 	tensor2 := NewTensor([]float64{4.0, 6.0, 3.0})
-	tensor1.Add(tensor2)
+	addError := tensor1.Add(tensor2)
 
 	if !utils.FloatSlicesEqual(tensor1.GetData(), []float64{5.0, 16.0, 8.0}) {
-		t.Errorf("Add result was incorrect, got %v, expected %v", tensor1.GetData(), []float64{5.0, 16.0, 8.0})
+		t.Errorf("add result was incorrect, got %v, expected %v", tensor1.GetData(), []float64{5.0, 16.0, 8.0})
+	} else if addError != nil {
+		t.Errorf("add result was correctly calculated, however error was returned when no error should occur")
+	}
+}
+
+//Test for the add functionality of tensors when the input is not valid (tensors of different sizes)
+func TestAddInvalid(t *testing.T) {
+	tensor1 := NewTensor([]float64{1.0, 10.0, 5.0, 6.0})
+	tensor2 := NewTensor([]float64{4.0, 6.0, 3.0})
+	addError := tensor1.Add(tensor2)
+
+	if addError == nil {
+		t.Errorf("add result was incorrect, got %v, expected error to occur due to different dimensions", tensor1.GetData())
+	}
+}
+
+//Test for the subtract functionality of tensors with valid inputs
+func TestSubtractValid(t *testing.T) {
+	tensor1 := NewTensor([]float64{4.0, 10.0, 5.0})
+	tensor2 := NewTensor([]float64{4.0, 6.0, 3.0})
+	subtractError := tensor1.Subtract(tensor2)
+
+	if !utils.FloatSlicesEqual(tensor1.GetData(), []float64{0.0, 4.0, 2.0}) {
+		t.Errorf("add result was incorrect, got %v, expected %v", tensor1.GetData(), []float64{0.0, 4.0, 2.0})
+	} else if subtractError != nil {
+		t.Errorf("add result was correctly calculated, however error was returned when no error should occur")
+	}
+}
+
+//Test for the subtract functionality of tensors when the input is not valid (tensors of different sizes)
+func TestSubtractInvalid(t *testing.T) {
+	tensor1 := NewTensor([]float64{1.0, 10.0, 5.0, 6.0})
+	tensor2 := NewTensor([]float64{4.0, 6.0, 3.0})
+	subtractError := tensor1.Subtract(tensor2)
+
+	if subtractError == nil {
+		t.Errorf("add result was incorrect, got %v, expected error to occur due to different dimensions", tensor1.GetData())
+	}
+}
+
+//Tests the dot product functionality for tensors when the input is valid
+func TestDotProductValid(t *testing.T) {
+	tensor1 := NewTensor([]float64{1.0, 10.0, 5.0})
+	tensor2 := NewTensor([]float64{4.0, 6.0, 3.0})
+	dotResult, dotError := tensor1.DotProduct(tensor2)
+
+	if dotResult != 79.0 || dotError != nil {
+		t.Errorf("dot product result was incorrect, got %f, expected %f", dotResult, 79.0)
+	} else if dotError != nil {
+		t.Errorf("Dot product result was correctly calculated, however error was returned when no error should occur")
+	}
+}
+
+//Tests the dot product functionality for tensors when the input is invalid (tensors of different sizes)
+func TestDotProductInvalid(t *testing.T) {
+	tensor1 := NewTensor([]float64{1.0, 10.0, 5.0, 6.0})
+	tensor2 := NewTensor([]float64{4.0, 6.0, 3.0})
+	dotResult, dotError := tensor1.DotProduct(tensor2)
+
+	if dotError == nil {
+		t.Errorf("dot product result was incorrect, got %f, expected error due to different dimensions", dotResult)
 	}
 }
