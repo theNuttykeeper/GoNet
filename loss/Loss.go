9db90ea045f4c38bb7aad4a4c99bc517f72b2eac
MichaelShep <mgs538@york.ac.uk>
2022-04-13 21:02:45 +0100

    Started to work on some of the loss functions

---
+++ loss/Loss.go
@@ -0,0 +1,34 @@
+package loss
+
+import (
+	"fmt"
+	"math"
+
+	"github.com/theNuttykeeper/GoNet/tensor"
+)
+
+//Function which calculates the Mean Squared Error loss for a given output
+func MSELoss(output, expected tensor.Tensor) (float64, error) {
+	if len(output.GetData()) != len(expected.GetData()) {
+		return 0.0, fmt.Errorf("expected and actual values not the same size, got sizes %d and %d", len(output.GetData()), len(expected.GetData()))
+	}
+
+	loss := 0.0
+	expectedData := expected.GetData()
+	for i, element := range output.GetData() {
+		loss += math.Pow((element - expectedData[i]), 2)
+	}
+
+	loss /= float64(len(expectedData))
+
+	return loss, nil
+}
+
+//Function which calculates the Binary Cross Entropy loss for a given output
+func BCELoss(output, expected tensor.Tensor) (float64, error) {
+	if len(output.GetData()) != len(expected.GetData()) {
+		return 0.0, fmt.Errorf("expected and actual values not the same size, got sizes %d and %d", len(output.GetData()), len(expected.GetData()))
+	}
+
+	return 0.0, nil
+}
